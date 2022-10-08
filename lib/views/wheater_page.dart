1ae8b42ce5bb525dcc01a9b85ca91540cf918b13
amilson gomes <gomes.amilson@gmail.com>
2022-10-07 22:23:02 -0300

    atualizado app

--- lib/views/wheater_page.dart
+++ lib/views/wheater_page.dart
@@ -13,38 +13,26 @@ class _WheaterPageState extends State<WheaterPage> {
 
 
   late final WeatherApi controller;
-  late final WeatherApi controller1;
+
 
   @override
   void initState() {
     controller = context.read<WeatherApi>();
-    controller1 = context.read<WeatherApi>();
+
     controller.getWeather();
-    controller1.getWeather1();
+
 
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final provider= Provider.of<WeatherApi>(context);
     return Scaffold(
          body:
              Column(
                children: [
                  Text(controller.json["results"]["city"]),
-                 const SizedBox(height: 10),
-                 Container(
-                   height: 300,
-                   width: 200,
-                   child: ListView.builder(
-                   itemCount: controller1.lista.length,
-                     itemBuilder: (context,index){
-                     return ListTile(
-                       title: Text(controller1.lista[index].toString()),
-                     );
-                     }),)
-
+                 Text(controller.json['results']['forecast'][1]['date'].toString())
 
                ],
              ));
