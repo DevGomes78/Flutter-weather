0d2d9438f4d40691f27de292e5baa410a6ddff89
usuario <gomes.amilson@gmail.com>
2022-11-02 05:32:54 -0300

    atualizado app

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,9 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wheater App'),
-      ),
+
   body: const WheaterPage(),
     );
   }
--- lib/views/wheater_page.dart
+++ lib/views/wheater_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wheater/models/wheater_models.dart';
+import '../components/list_data.dart';
 import '../service/wheater_api_service.dart';
 import 'package:provider/provider.dart';
+
 class WheaterPage extends StatefulWidget {
   const WheaterPage({Key? key}) : super(key: key);
 
@@ -10,33 +12,88 @@ class WheaterPage extends StatefulWidget {
 }
 
 class _WheaterPageState extends State<WheaterPage> {
-
-
-  late final WeatherApi controller;
-
+  WeatherApi controller = WeatherApi();
 
   @override
   void initState() {
     controller = context.read<WeatherApi>();
-
     controller.getWeather();
-
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         body:
-             Column(
-               children: [
-                 Text(controller.json["results"]["city"]),
-                 Text(controller.json['results']['forecast'][1]['date'].toString())
+      body: FutureBuilder<void>(
+        future: controller.getWeather(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('erro na pagina'),
+                );
+              } else {
+                return Column(
+                  children: [
+                    _topcard(),
+                    const SizedBox(height: 10),
 
-               ],
-             ));
+                     ListData(),
+                  ],
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
 
-
+  _topcard() {
+    return Container(
+      alignment: Alignment.center,
+      height: 200,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20), color: Colors.blue),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            controller.json['results']['city'],
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${controller.json['results']['temp']} °',
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            controller.json['results']['description'],
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
