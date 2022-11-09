187736f1bc81ecb1cf6ea166e0926ab56f9eb27e
amilson gomes <gomes.amilson@gmail.com>
2022-11-09 11:54:52 -0300

    atualizado app

--- lib/service/wheater_api_service.dart
+++ lib/service/wheater_api_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../models/wheater_models.dart';
+
+import '../models/weather_model.dart';
+
 
 class WeatherApi extends ChangeNotifier {
   var json;
