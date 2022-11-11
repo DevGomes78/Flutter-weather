37f45bbfcaef2468c098497b4a170e3e5c7c73a8
amilson gomes <gomes.amilson@gmail.com>
2022-11-11 11:56:10 -0300

    atualizado cor do app

--- lib/views/wheater_page.dart
+++ lib/views/wheater_page.dart
@@ -3,7 +3,6 @@ import '../components/botton_card.dart';
 import '../components/top_card.dart';
 import '../components/weather_list.dart';
 
-
 class WheaterPage extends StatefulWidget {
   const WheaterPage({Key? key}) : super(key: key);
 
@@ -15,22 +14,20 @@ class _WheaterPageState extends State<WheaterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Colors.white,
+      backgroundColor: Colors.white,
       body: Container(
         decoration: const BoxDecoration(
           color: Colors.black,
-
-
-
-
         ),
         child: ListView(
           children: const [
             TopCard(),
             Divider(color: Colors.white),
             WeatherList(),
+            Divider(color: Colors.white),
             SizedBox(height: 20),
             BottonCard(),
+            Divider(color: Colors.white),
           ],
         ),
       ),
