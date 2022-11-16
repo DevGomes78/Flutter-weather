09a458ad941b272e61c5bee19124121469e51ac7
amilson gomes <gomes.amilson@gmail.com>
2022-11-16 14:18:46 -0300

    atualizado string_constants.dart

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -1,7 +1,7 @@
 class StringConstants{
   static const errorPage = 'erro na pagina';
   static const forecastForWeek= ' Previsao para semana';
-  static const clearWeather = 'Tempo Limpo';
+  static const clearWeather = 'Tempo limpo';
   static const Rain = 'Chuva';
   static const scatteredRains = 'Chuvas esparsas';
   static const partlyCloud = 'Parcialmente nublado';
