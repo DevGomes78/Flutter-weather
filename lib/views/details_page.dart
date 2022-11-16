457d2171175577d508b02fcf1f761167cb15ace9
amilson gomes <gomes.amilson@gmail.com>
2022-11-16 14:53:40 -0300

    criado funcao _details no arquivo details_page.dart

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -24,115 +24,119 @@ class _DetailsPageState extends State<DetailsPage> {
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
                 colors: [Colors.black, Colors.blueAccent])),
-        child: Stack(
-          children: [
-            Positioned(
-              left: 20,
-              top: 40,
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: (Icon(
-                  Icons.arrow_back_ios_new,
-                  color: Colors.white,
-                  size: 30,
-                )),
-              ),
+        child: _details(context),
+      ),
+    );
+  }
+
+  _details(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          left: 20,
+          top: 40,
+          child: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: (const Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.white,
+              size: 30,
+            )),
+          ),
+        ),
+        const Positioned(
+          left: 20,
+          top: 100,
+          child: Text(
+            StringConstants.state,
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            const Positioned(
-              left: 20,
-              top: 100,
-              child: Text(
-                'São Paulo,Sp',
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 140,
-              child: Text(
-                ' ${widget.forecast.date.toString()},',
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            ),
-            Positioned(
-              left: 90,
-              top: 140,
-              child: Text(
-                widget.forecast.weekday.toString(),
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-            ),
-            SizedBox(width: 20),
-            Positioned(
-              left: 100,
-              top: 250,
-              child: (widget.forecast.description.toString() ==
-                      StringConstants.clearWeather
+          ),
+        ),
+        Positioned(
+          left: 20,
+          top: 140,
+          child: Text(
+            ' ${widget.forecast.date.toString()},',
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+        Positioned(
+          left: 90,
+          top: 140,
+          child: Text(
+            widget.forecast.weekday.toString(),
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+        const SizedBox(width: 20),
+        Positioned(
+          left: 100,
+          top: 250,
+          child: (widget.forecast.description.toString() ==
+                  StringConstants.clearWeather
+              ? const Icon(
+                  Icons.sunny,
+                  size: 200,
+                  color: Colors.amber,
+                )
+              : widget.forecast.description.toString() ==
+                      StringConstants.partlyCloud
                   ? const Icon(
-                      Icons.sunny,
+                      Icons.cloud,
                       size: 200,
-                      color: Colors.amber,
+                      color: Colors.grey,
                     )
                   : widget.forecast.description.toString() ==
-                          StringConstants.partlyCloud
+                          StringConstants.Rain
                       ? const Icon(
-                          Icons.cloud,
+                          Icons.cloudy_snowing,
                           size: 200,
                           color: Colors.grey,
                         )
                       : widget.forecast.description.toString() ==
-                              StringConstants.Rain
+                              StringConstants.scatteredRains
                           ? const Icon(
-                              Icons.cloudy_snowing,
+                              Icons.sunny_snowing,
+                              size: 200,
+                              color: Colors.amber,
+                            )
+                          : const Icon(
+                              Icons.cloud,
                               size: 200,
                               color: Colors.grey,
-                            )
-                          : widget.forecast.description.toString() ==
-                                  StringConstants.scatteredRains
-                              ? const Icon(
-                                  Icons.sunny_snowing,
-                                  size: 200,
-                                  color: Colors.amber,
-                                )
-                              : const Icon(
-                                  Icons.cloud,
-                                  size: 200,
-                                  color: Colors.grey,
-                                )),
-            ),
-            Positioned(
-              left: 120,
-              top: 470,
-              child: Text(
-                widget.forecast.description.toString(),
-                style: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 150,
-              top: 520,
-              child: Text(
-                ' ${widget.forecast.max.toString()}°',
-                style: const TextStyle(
-                  fontSize: 60,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ],
+                            )),
         ),
-      ),
+        Positioned(
+          left: 120,
+          top: 470,
+          child: Text(
+            widget.forecast.description.toString(),
+            style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 150,
+          top: 520,
+          child: Text(
+            ' ${widget.forecast.max.toString()}°',
+            style: const TextStyle(
+              fontSize: 60,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
